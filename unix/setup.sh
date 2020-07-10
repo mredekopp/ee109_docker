99f86f3a67b68b35e176e94c6f7645301c0d78fb
Mark Redekopp <redekopp@usc.edu>
2020-07-10 13:35:26 -0700

    Added Unix and Windows

---
+++ unix/setup.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+manage="./manage.sh"
+
+# Go to docker folder
+cd "$(cd "$(dirname "$(dirname "${BASH_SOURCE[0]}")")" || return >/dev/null 2>&1 && pwd)" || return
+
+# Check for docker on path
+echo "Looking for docker..."
+command -v docker >/dev/null 2>&1 || {
+  echo "Docker is not installed or available on path!"
+  exit 1
+}
+
+# Create docker image
+echo "Creating docker image..."
+docker build -t csci104 -f ./Dockerfile . || exit $?
+
+# Get mount point
+echo "Creating manager script..."
+read -r -p "Select a directory to mount: " work
+
+while [[ ! -d "${work}" ]]; do
+  echo "Invalid directory!"
+  read -r -p "Select a directory to mount: " work
+done
+
+# Get absolute path of directory
+work="$(cd "${work}" || return; pwd -P)"
+echo "Mount point set, this can be changed later by editing manage.sh..."
+
+# Create manage scripts
+echo "Creating manager script..."
+rm ${manage} 2> /dev/null
+{
+  echo "#!/usr/bin/env bash";
+  echo ""
+  echo "# Change this to the directory you want to access in your container."
+  echo "work=${work}"
+  echo ""
+  cat ./unix/manage.base.sh
+} >> ${manage};
+chmod +x ${manage}
+
+echo "Done!"
---
+++ windows/setup.bat
@@ -0,0 +1,65 @@
+@echo off
+
+set manage=".\manage.bat"
+echo "%manage%"
+
+REM Go to docker folder
+pushd %0\..
+
+REM Check for docker on path
+@echo Looking for docker...
+where docker 1>nul 2>nul
+if errorlevel 1 (
+  echo Docker is not installed or available on path!
+  exit 1
+)
+
+REM Create docker image
+echo Creating docker image...
+docker build -t csci104 -f ..\Dockerfile ..
+
+REM Get mount point
+echo Creating manager script...
+set /p work="Select a directory to mount: "
+
+:invalid
+if not exist %work% (
+  echo Invalid directory!
+  set /p work="Select a directory to mount: "
+  goto invalid
+)
+
+REM Remove trailing backslash
+IF %work:~-1%==\ SET work=%work:~0,-1%
+
+REM Directory should be relative to caller
+popd
+
+REM Get absolute path of directory
+call :absolute "%work%"
+set work="%return%"
+echo Mount point set, this can be changed later by editing manage.bat...
+
+REM Go to docker folder
+pushd %0\..\..
+
+REM Create manage script
+echo Creating manager script...
+del "%manage%"
+>"%manage%" (
+  echo @echo off
+  echo(
+  echo REM Change this to the directory you want to access in your container
+  echo set work=%work%
+  echo(
+  type .\windows\manage.base.bat
+)
+
+REM
+echo Done!
+exit /B
+
+REM convert path to absolute
+:absolute
+set return=%~dpfn1
+exit /B
